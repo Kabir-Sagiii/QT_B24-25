52b92fdf98b3e06a6498222e9c47756a1562ebfe
Kabir <sagithedeveloper@gmail.com>
2025-06-03 14:53:08 +0530

    Added

---
+++ MongoDB/S10/First.js
@@ -0,0 +1,9 @@
+function f1() {
+  console.log("f1 is called");
+}
+
+function f2() {
+  console.log("f2 is called");
+}
+
+module.exports = { f1, f2 };
---
+++ MongoDB/S10/Second.js
@@ -0,0 +1,5 @@
+const obj = require("./First.js");
+
+obj.f1();
+obj.f2();
+// console.log(module);
